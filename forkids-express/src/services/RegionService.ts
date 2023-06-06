f564a581d3bbea99bf68a791475f20d0800d046d
ehdwoKIM <kinbell19@gmail.com>
2023-06-06 13:00:10 +0900

    [FIX] 지역별 핫플레이스 조회 기능 수정

--- forkids-express/src/services/RegionService.ts
+++ forkids-express/src/services/RegionService.ts
@@ -7,21 +7,17 @@ const getPlaceByGu = async (
   try {
     guName = guName.trim();
     
-    const places = await Place
-        .find({ address: new RegExp(guName, 'i') })
-        .select('_id')
-        .exec();
+    const places = await Place.find({ address: new RegExp(guName, 'i') })
 
-    console.log(places)
-
-    if (places.length > 0) {
-        const data = places.map(place => {
-            return { id: place._id.toString() }; // MongoDB의 _id는 ObjectId 형식이므로 toString()을 사용해 문자열로 변환합니다.
-        });
-        return data;
+    if (!places) {
+      return null;
     }
-
-    return null;
+    const data = places.map(place => ({
+      id: place._id,
+      name: place.name,
+    }));
+    
+    return data;
 
   } catch (error) {
     console.log(error);
